1b7285176a0fff3712e96c8081f596a5951bde92
Ekaterina Polyakova <pereletnaya@mail.ru>
2024-05-02 22:53:54 +0300

    добавили операции

---
+++ Task2/Program.cs
@@ -0,0 +1,15 @@
+﻿int i1 = 5;
+int i2 = 6;
+int sum = i1 +i2;
+Console.WriteLine(sum);
+
+long l1 = 6;
+long sum2 = l1 + i1;
+Console.WriteLine(sum2);
+
+int product1 = i1 * i2;
+long product2 = l1 * i1;
+long product3 = l1 * i2;
+Console.WriteLine(product1);
+Console.WriteLine(product2);
+Console.WriteLine(product3); 
